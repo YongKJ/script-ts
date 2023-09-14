3cf0ba4ffcc1f44ac72868a0a12c4d1c2ff320a7
dxj <1718874198@qq.com>
2023-09-14 14:17:12 +0800

    [update]puppeteer test

--- src/application/applet/puppeteer-test/PuppeteerTest.ts
+++ src/application/applet/puppeteer-test/PuppeteerTest.ts
@@ -1,4 +1,5 @@
 import {GenUtil} from "../../util/GenUtil";
+import puppeteer from "puppeteer-core";
 
 export class PuppeteerTest {
 
@@ -9,12 +10,45 @@ export class PuppeteerTest {
         this.chromePath = GenUtil.anyToStr(value);
     }
 
-    private apply(): void {
+    private async apply(): Promise<void> {
+        const browser = await puppeteer.launch({
+            executablePath: this.chromePath,
+            args: ['--start-maximized'],
+            headless: false,
+            timeout: 15000
+        });
 
+        const page = await browser.newPage();
+        await page.setViewport({
+            width: 1920,
+            height: 1280,
+            deviceScaleFactor: 1,
+        });
+        await page.goto('https://baidu.com');
+
+        const dimensions = await page.evaluate(() => {
+            return {
+                width: document.documentElement.clientWidth,
+                height: document.documentElement.clientHeight,
+                deviceScaleFactor: window.devicePixelRatio
+            };
+        });
+        await page.screenshot({
+            path: 'C:\\Users\\Admin\\Desktop\\png\\baidu.png',
+            clip: {
+                x: 0,
+                y: 0,
+                width: dimensions.width,
+                height: dimensions.height
+            }
+        });
+
+        await GenUtil.sleep(10000);
+        await browser.close();
     }
 
     public static run(): void {
-        new PuppeteerTest().apply();
+        new PuppeteerTest().apply().then();
     }
 
 }
